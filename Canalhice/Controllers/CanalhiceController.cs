5a92f17fb969aaddfdf5e0d6ccb2e2fd854d3146
David Wesley Claudino Gonçalves <david_wesleygo@hotmail.com>
2021-02-20 12:29:44 -0300

    Add project files.

---
+++ Canalhice/Controllers/CanalhiceController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace canalhice.api.Controllers
+{
+    [Route("api/canalhice")]
+    [ApiController]
+    public class CanalhiceController : ControllerBase
+    {
+        [HttpGet, Route("testandoApi")]
+        public string GetTestandoApi()
+        {
+            Response.StatusCode = (int)HttpStatusCode.OK;
+
+            return "coé rapaziada";
+        }
+        [HttpGet]
+        public string RecebendoDisparando(string texto)
+        {
+            return new string(texto.Reverse().ToArray());
+        }
+    }
+}
